7ea9502d9182ab5c93f3f4b07ecf397f17ae9ca6
Chopfl <claudiahopfl@gmail.com>
2019-02-11 23:14:40 +0100

    Tabela Model

---
+++ SalonSamochodowy/dbo/Tables/Model.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Model] (
+    [ID]                INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwa]             VARCHAR (255) NOT NULL,
+    [IloscDrzwi]        INT           NOT NULL,
+    [ZamekCentralny]    BIT           NOT NULL,
+    [Radio]             BIT           NOT NULL,
+    [PoczatekProdukcji] DATE          NOT NULL,
+    [MasaKg]            INT           NOT NULL,
+    [Marka]             VARCHAR (255) DEFAULT ('Nieznana') NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    FOREIGN KEY ([Marka]) REFERENCES [dbo].[Marka] ([Nazwa]) ON DELETE SET DEFAULT
+);
+
