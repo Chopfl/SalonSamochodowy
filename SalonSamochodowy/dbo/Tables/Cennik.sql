6cc14c9fe7dfbe33f6a3a3343332a9a4fcfd7e99
Chopfl <claudiahopfl@gmail.com>
2019-02-11 23:34:22 +0100

    Tabela Cennik

---
+++ SalonSamochodowy/dbo/Tables/Cennik.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Cennik] (
+    [ID]       INT             IDENTITY (1, 1) NOT NULL,
+    [ModelID]  INT             NULL,
+    [Cena]     DECIMAL (19, 2) NOT NULL,
+    [Uzywany]  BIT             NOT NULL,
+    [Przebieg] INT             NOT NULL,
+    [CenaOd]   DATE            NOT NULL,
+    [CenaDo]   DATE            NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    FOREIGN KEY ([ModelID]) REFERENCES [dbo].[Model] ([ID]) ON DELETE SET NULL
+);
+
