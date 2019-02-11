e6da03f150fcd3957cc9f93b8193333138ea041f
Chopfl <claudiahopfl@gmail.com>
2019-02-12 00:43:19 +0100

    Tabele Stanowisko pracy oraz Zespol

---
+++ SalonSamochodowy/dbo/Tables/StanowiskoPracy.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[StanowiskoPracy] (
+    [ID]        INT           IDENTITY (1, 1) NOT NULL,
+    [Nazwa]     VARCHAR (255) NOT NULL,
+    [Obowiazki] VARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
