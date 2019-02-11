06260c16bca8fcfecc927554b7f4ef472f103159
Chopfl <claudiahopfl@gmail.com>
2019-02-11 23:49:03 +0100

    Tabele Pracownik i Klient

---
+++ SalonSamochodowy/dbo/Tables/Klient.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Klient] (
+    [Imie]               VARCHAR (255) NOT NULL,
+    [Nazwisko]           VARCHAR (255) NOT NULL,
+    [NrTel]              INT           NULL,
+    [Adres]              VARCHAR (255) NOT NULL,
+    [Email]              VARCHAR (255) NOT NULL,
+    [NrDowoduOSobistego] VARCHAR (10)  NOT NULL,
+    UNIQUE NONCLUSTERED ([NrDowoduOSobistego] ASC)
+);
+
---
+++ SalonSamochodowy/dbo/Tables/Pracownik.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Pracownik] (
+    [ID]                  INT           IDENTITY (1, 1) NOT NULL,
+    [Imie]                VARCHAR (255) NOT NULL,
+    [Nazwisko]            VARCHAR (255) NOT NULL,
+    [PESEL]               INT           NOT NULL,
+    [NrKontaBankowego]    INT           NOT NULL,
+    [DataPodpisaniaUmowy] DATE          NOT NULL,
+    [CzyNaWypowiedzeniu]  BIT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    UNIQUE NONCLUSTERED ([PESEL] ASC)
+);
+
