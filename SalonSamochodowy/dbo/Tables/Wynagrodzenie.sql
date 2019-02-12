2afdd9a3e736d892750c3bd9a4453fab498d9df5
Chopfl <claudiahopfl@gmail.com>
2019-02-12 01:13:52 +0100

    Tabele Wynagrodzenie i Wypłata

---
+++ SalonSamochodowy/dbo/Tables/Wynagrodzenie.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Wynagrodzenie] (
+    [ID]          INT             IDENTITY (1, 1) NOT NULL,
+    [Kwota]       DECIMAL (19, 4) NOT NULL,
+    [OkresOd]     DATE            NOT NULL,
+    [OkresDo]     DATE            NOT NULL,
+    [PracownikID] INT             DEFAULT ((-1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CHECK ([Kwota]>(0)),
+    FOREIGN KEY ([PracownikID]) REFERENCES [dbo].[Pracownik] ([ID]) ON DELETE SET DEFAULT
+);
+
---
+++ SalonSamochodowy/dbo/Tables/Wyplata.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Wyplata] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [PracownikID] INT           DEFAULT ((-1)) NOT NULL,
+    [KwotaID]     INT           DEFAULT ((-1)) NOT NULL,
+    [DataWyplaty] DATE          NOT NULL,
+    [Typ]         VARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    CHECK ([Typ]='premia' OR [Typ]='pensja'),
+    FOREIGN KEY ([KwotaID]) REFERENCES [dbo].[Wynagrodzenie] ([ID]) ON DELETE SET DEFAULT,
+    FOREIGN KEY ([PracownikID]) REFERENCES [dbo].[Pracownik] ([ID]) ON DELETE SET DEFAULT
+);
+
