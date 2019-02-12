7471029eeaaa42cc1f80e5f24fb37d33d635d810
Chopfl <claudiahopfl@gmail.com>
2019-02-13 00:18:01 +0100

    Widok dla HR: roczne wynagrodzenie pracowników

---
+++ SalonSamochodowy/rapOp/Views/vwRocznePensje.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW rapOp.vwRocznePensje as
+	--Srednia Roczna Pensja Pracownika   
+	SELECT p.Imie, p.Nazwisko, SUM(w.Kwota * 12) AS RocznaPensja  
+	From Pracownik AS p LEFT JOIN Hr.Wynagrodzenie AS w On p.ID = w.PracownikID
+	GROUP BY p.Imie, p.Nazwisko;
