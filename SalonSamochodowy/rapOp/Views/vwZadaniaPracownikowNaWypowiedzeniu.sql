a918106ac4b59548b15d75939327281295f17812
Chopfl <claudiahopfl@gmail.com>
2019-02-12 23:49:46 +0100

    Widok dla Serwisu: Zamowienia na przegląd przypisane do pracowników na wypowiedzeniu

---
+++ SalonSamochodowy/rapOp/Views/vwZadaniaPracownikowNaWypowiedzeniu.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW rapOp.vwZadaniaPracownikowNaWypowiedzeniu as
+	--Zamowienia na przeglad wykonane przez pracownikow na wypowiedzeniu   
+	SELECT u.Nazwa, p.Nazwisko as NazwiskoPracownikaNaWypowiedzeniu 
+	FROM Serwis.UslugaSerwisu AS u LEFT JOIN Serwis.ZamowienieSerwisu AS z ON u.ID = z.UslugaID LEFT JOIN Pracownik AS p ON z.PracownikID = p.ID
+	WHERE u.Nazwa = 'Przeglad' AND p.CzyNaWypowiedzeniu = 1
+	GROUP BY u.Nazwa, p.Nazwisko;
