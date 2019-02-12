cb4790230b9bba542e217c045dd070255e81a946
Chopfl <claudiahopfl@gmail.com>
2019-02-12 23:25:41 +0100

    Widok dla Serwisu: zamowienia zamkniete

---
+++ SalonSamochodowy/rapOp/Views/vwZamowieniaSerwisuZakonczone.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW rapOp.vwZamowieniaSerwisuZakonczone as
+	--zamowienia uslug z serwisu o statusie zamkniete 
+	SELECT z.ID
+	FROM Serwis.HistoriaZamowieniaSerwisu AS z LEFT JOIN Serwis.StatusZamowieniaSerwisu AS s ON s.ID = z.StatusZamowieniaSerwisuID
+	WHERE s.NazwaStatusu = 'zamkniete';
