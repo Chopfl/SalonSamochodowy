6f9f010067f1614aff5797771b3d503a3dcde408
Chopfl <claudiahopfl@gmail.com>
2019-02-12 23:04:05 +0100

    Widok ZamowieniaRabat

---
+++ SalonSamochodowy/rapOp/Views/vwZamowieniaRabat.sql
@@ -0,0 +1,3 @@
+﻿CREATE VIEW rapOp.vwZamowieniaRabat as
+	SELECT z.ID, z.Cena, c.Cena as CenaKatalogowa, ((c.Cena-z.Cena)/c.Cena) as Rabat
+	FROM Zamowienie AS z LEFT JOIN Cennik AS c ON c.ID = z.CennikID;
