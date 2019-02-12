935ffa68413bfb0bc55df88074e28646628bf386
Chopfl <claudiahopfl@gmail.com>
2019-02-12 01:53:10 +0100

    Tabele z obszaru Serwis: usługi, status, zamówienie oraz historia zamówień.

---
+++ SalonSamochodowy/dbo/Tables/UslugaSerwisu.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UslugaSerwisu] (
+    [ID]    INT             IDENTITY (1, 1) NOT NULL,
+    [Nazwa] VARCHAR (255)   NOT NULL,
+    [Koszt] DECIMAL (19, 4) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
