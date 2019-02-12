34dac337492f442d096663d4ca3efa8bc749db6d
Chopfl <claudiahopfl@gmail.com>
2019-02-12 23:39:27 +0100

    Widok dla Serwisu, ilość zamówień na przegląd samochodu

---
+++ SalonSamochodowy/rapOp/Views/vwZamowieniaSerwisuNaPrzeglad.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW rapOp.vwZamowieniaSerwisuNaPrzeglad as
+	--Ilosc zamowienia przegladu w serwisie   
+	SELECT u.Nazwa,  COUNT(z.ID) as IlosZamowien
+	FROM Serwis.UslugaSerwisu AS u LEFT JOIN Serwis.ZamowienieSerwisu AS z ON u.ID = z.UslugaID
+	WHERE u.Nazwa = 'Przeglad'
+	GROUP BY u.Nazwa;
