2badd6a0a1dc4c2ff5211ee6b4464aa3aab53018
Chopfl <claudiahopfl@gmail.com>
2019-02-12 01:03:26 +0100

    Tabele PracownikStanowisko oraz PracownikZespol

---
+++ SalonSamochodowy/dbo/Tables/PracownikZespol.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[PracownikZespol] (
+    [ID]          INT IDENTITY (1, 1) NOT NULL,
+    [PracownikID] INT DEFAULT ((-1)) NOT NULL,
+    [ZespolID]    INT DEFAULT ((-1)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+    FOREIGN KEY ([PracownikID]) REFERENCES [dbo].[Pracownik] ([ID]) ON DELETE SET DEFAULT,
+    FOREIGN KEY ([ZespolID]) REFERENCES [dbo].[Zespol] ([ID]) ON DELETE SET DEFAULT
+);
+
