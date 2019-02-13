9d03485fd3dafbaf1697e01888c1d86e1565cbb3
Chopfl <claudiahopfl@gmail.com>
2019-02-13 23:42:07 +0100

    Procedura dodająca wybraną ilość losowych zamówień w serwisie

---
+++ SalonSamochodowy/dbo/Stored Procedures/DodajZamowieniaDoSerwis.sql
@@ -0,0 +1,25 @@
+﻿-- Procedura dodawania losowych zamowien serwisu
+
+CREATE PROCEDURE DodajZamowieniaDoSerwis 
+ @ileWierszy int
+as
+
+BEGIN
+
+	DECLARE @iteracjaPetli as int 
+	SET @iteracjaPetli = 0
+
+	WHILE @iteracjaPetli < @ileWierszy
+	BEGIN
+
+		DECLARE @PracownikID as int = (SELECT TOP 1 ID FROM Pracownik ORDER BY NEWID())
+		DECLARE @UslugaID as int = (SELECT TOP 1 ID FROM Serwis.UslugaSerwisu ORDER BY NEWID())
+		DECLARE @KlientID as int = (SELECT TOP 1 ID FROM Klient ORDER BY NEWID())
+		
+		INSERT INTO Serwis.ZamowienieSerwisu (PracownikID, UslugaID, KlientID)
+		VALUES
+		(@PracownikID, @UslugaID, @KlientID) 
+		
+	END
+
+END
