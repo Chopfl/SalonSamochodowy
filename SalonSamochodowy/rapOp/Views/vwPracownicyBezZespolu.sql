ec3ec895b71004be31afb750ef884cde0e824711
Chopfl <claudiahopfl@gmail.com>
2019-02-13 16:30:11 +0100

    Widok dla Hr: pracownicy nie przypisani do zespolu

---
+++ SalonSamochodowy/rapOp/Views/vwPracownicyBezZespolu.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW rapOp.vwPracownicyBezZespolu as
+	--Pracownicy nie przydzieleni do zespolow   
+	SELECT p.ID, p.Imie, p.Nazwisko
+	FROM Pracownik AS p INNER JOIN Hr.PracownikZespol AS pz ON p.ID = pz.PracownikID
+	WHERE pz.PracownikID IS NULL;
